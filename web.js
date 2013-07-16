c739350fd2ebd55241ea6801514b0eb1c7cd0175
kiser <kipot.sergey@gmail.com>
2013-07-16 15:11:03 +0000

    fix

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 var buffer = fs.readFileSync(filename);
 
 app.get('/', function(request, response) {
-  response.send( buffer.toString );
+  response.send( buffer.toString() );
 });
 
 var port = process.env.PORT || 5000;
