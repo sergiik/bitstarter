ab5855e9137269fb516ac4761de44c82214980fa
kiser <kipot.sergey@gmail.com>
2013-07-16 14:46:48 +0000

    Reading message from index.html added

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var filename = 'index.html';
 
 var app = express.createServer(express.logger());
+var buffer = fs.readFileSync(filename);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send( buffer.toString );
 });
 
 var port = process.env.PORT || 5000;
